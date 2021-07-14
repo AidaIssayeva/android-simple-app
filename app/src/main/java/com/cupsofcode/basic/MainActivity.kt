a90ffc1788b715914721e08feb854773c8c295e4
Aida <issayeva.aida@gmail.com>
2021-07-13 22:54:01 -0400

    first commit

---
+++ app/src/main/java/com/cupsofcode/basic/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.cupsofcode.basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    button.setOnClickListener {
+      textView.text = getRandomFruit()
+    }
+  }
+
+  private val fruits = listOf(
+      "Apple",
+      "Pear",
+      "Pineapple",
+      "Strawberry",
+      "Melon",
+      "Blueberry"
+  )
+
+  private fun getRandomFruit(): String {
+    return fruits.random()
+  }
+
+}
